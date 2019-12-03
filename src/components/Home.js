95320ef6c0b16845a551d1ba3e9063aa235b01fa
Stanley Chen <dannyshien@gmail.com>
2019-12-03 17:34:16 -0500

    Initial commit for Phony Movie with basic structure of app

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavBar from './NavBar'
+
+const PhonyMovie = () => {
+    return (
+        <div>
+            <NavBar />
+            <div>Different movie categories</div>
+            <div>Different movie categories</div>
+            <div>Different movie categories</div>
+            <div>Different movie categories</div>
+            <div>Different movie categories</div>
+        </div>
+    )
+}
+
+export default PhonyMovie; 
---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div>
+            <div>Phony Movies</div>
+            <div>
+                <p>Home</p>
+                <p>TV Shows</p>
+                <p>Movies</p>
+                <p>My List</p>
+            </div>
+            <div>
+                <p>search bar</p>
+                <p>settings</p>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar; 
