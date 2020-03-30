3dc2599a27c52cb9d9942a31d14fd1011fbe4c55
Stanley Chen <dannyshien@gmail.com>
2020-03-30 01:53:42 -0400

    Small restructure of file architecture

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import NavBar from '././components/Navbar/NavBar';
-import PhonyMovie from './containers/Landing/PhonyMovie';
+import NavBar from './components/navbar/NavBar';
+import PhonyMovie from './containers/landing/PhonyMovie';
 
 
 const App = () => {
--- src/containers/Landing/PhonyMovie.js
+++ src/containers/Landing/PhonyMovie.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from './node_modules/react';
 // import { Link } from 'react-router-dom';
 import Genre from '../../components/genre/GenreDisplay';
 import './PhonyMovie.css';
