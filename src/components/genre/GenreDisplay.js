1499a30ac908f4a99a1039b1908a937e9581af2e
Stanley Chen <dannyshien@gmail.com>
2020-04-09 17:55:15 -0400

    Remove current styling for displaying information to create new style

--- src/components/genre/GenreDisplay.js
+++ src/components/genre/GenreDisplay.js
@@ -5,49 +5,35 @@ import FakePoster from '../fakePoster/FakePoster';
 
 const GenreDisplay = ({ genreType, type, headerText, style }) => {
 
-    let movieGenres = genreType.map((genreList, i) => {
+  let movieGenres = genreType.map((genreList, i) => {
 
-        let id = genreList.id
-        let title = genreList.title
-        let poster = genreList.poster_path
-        let summary = genreList.overview
-        let release = genreList.release_date
-        let rating = genreList.vote_average
-
-        return (
-            // TODO: Make each image poster a link that pops up a "modal", with more details to that movie. 
-            <div key={ id } className='posterWrapper' >
-                { poster === null ? 
-                <FakePoster 
-                    // src= { `https://image.tmdb.org/t/p/w154/${ poster }` }
-                    alt={ `${ title } poster` } 
-                    text={ `${ title }` } 
-                /> :
-                // TODO: Get image src as background image. 
-                <MoviePoster 
-                    src={ `https://image.tmdb.org/t/p/w300/${ poster }` } 
-                    alt={ `${ title } poster` } 
-                    title={ title }
-                    summary={ summary }
-                    release={ release }
-                    rating={ rating }
-                /> }
-            </div>
-        )
-    })
+    let id = genreList.id
+    let title = genreList.title
+    let poster = genreList.poster_path 
     
     return (
-        <div className='genre' type={ type }>
-            <h2 style={ style }>{ headerText }</h2>
-            {/* 
-                TODO: Look into carousel css, 
-                make margin-right transparent
-            */}
-            <div className='genreCarousel'>
-                { movieGenres }
-            </div>
-        </div>
+      <div key={ id } className='posterWrapper' >
+        { poster === null ? 
+        <FakePoster 
+          src= { `https://image.tmdb.org/t/p/w154/${ poster }` }
+          alt={ `${ title } poster` } 
+        /> : 
+        <MoviePoster 
+          src={ `https://image.tmdb.org/t/p/w154/${ poster }` } 
+          alt={ `${ title } poster` } 
+        /> }
+      </div>
     )
+  })
+    
+  return (
+    <div className='genre' type={ type }>
+      <h2 style={ style }>{ headerText }</h2>
+      <div className='genreCarousel'>
+        { movieGenres }
+      </div>
+    </div>
+  )
 }
     
 
