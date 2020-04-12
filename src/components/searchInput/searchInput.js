f4f80c0318459febe7acfece9b731a3a8d4d5710
Stanley Chen <dannyshien@gmail.com>
2020-04-11 23:06:59 -0400

    Set onChange and onSubmit handler

--- src/components/searchInput/searchInput.js
+++ src/components/searchInput/searchInput.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
 
 class SearchInput extends Component {
-  state = { query='' }
+  state = { query: '' }
 
   render() {
     return(
-      <form>
+      <form onSubmit={ this.onQuerySubmit } >
         <label>
           <input 
             type='text'
             name='query'
             placeholder='Search for movies'
             vlaue={ this.state.query }
+            onQueryInput= { this.onQueryInput }
           />
         </label>
       </form>
     )
   }
+
+  onQueryInput = (e) => {
+    this.setState = ({
+      [e.target.name] : e.target.value
+    })
+  }
+
+  onQuerySubmit = () => {
+    e.preventDefault() 
+
+
+  }
+
 }
 
 export default SearchInput; 
