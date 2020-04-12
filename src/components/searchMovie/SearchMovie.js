71c357405738e71490baff9d52f5915f925c0cee
Stanley Chen <dannyshien@gmail.com>
2020-04-12 15:53:21 -0400

    able to fetch api

--- src/components/searchMovie/SearchMovie.js
+++ src/components/searchMovie/SearchMovie.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import './SearchMovie.css';
+import axios from 'axios';
 
 import SearchForm from '../searchForm/SearchForm';
 
 class SearchMovie extends Component {
-  state = {  movie: {} }
+  state = { movie: {}, genreIds: [] }
 
   conponentDidMount () {
     this.findMovie()
   }
 
-  findMovie(query) {
-    // const TMDB = `${process.env.REACT_APP_TMDB_KEY}`
-
+  async findMovie(query) {
+    const TMDB = `${process.env.REACT_APP_TMDB_KEY}`
+    const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${TMDB}&language=en-US&query=${query}&page=1&include_adult=false`);
+    console.log(response.data.results)
+    // Not able to setState??? 
+    // this.setState({
+    //   movie: response.data.results
+    // })
   }
 
   render() {
