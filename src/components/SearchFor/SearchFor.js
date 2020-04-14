788caae7e834bc78994ccd9d7efc4c1db37c042f
Stanley Chen <dannyshien@gmail.com>
2020-04-14 13:10:54 -0400

    Rename component to SearchFor, add route and setState in component

--- src/components/SearchFor/SearchFor.js
+++ src/components/SearchFor/SearchFor.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
-import './SearchMovie.css';
+import './MovieSearch.css';
 import axios from 'axios';
 
 import SearchForm from '../searchForm/SearchForm';
 
-class SearchMovie extends Component {
-  state = { movie: {}, genreIds: [] }
+class SearchFor extends Component {
+  state = { movie: [], genreIds: [] }
 
   conponentDidMount () {
     this.findMovie()
   }
 
-  async findMovie(query) {
+  findMovie = async query => {
     const TMDB = `${process.env.REACT_APP_TMDB_KEY}`
     const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${TMDB}&language=en-US&query=${query}&page=1&include_adult=false`);
     console.log(response.data.results)
-    // Not able to setState??? 
-    // this.setState({
-    //   movie: response.data.results
-    // })
-  }
+    this.setState({
+      movie: response.data.results
+    });
+  };
 
   render() {
+    console.log(`NEW STATE: `, this.state.movie)
     return(
       <div className='search'>
         <SearchForm fetch={ this.findMovie } />
@@ -34,4 +34,4 @@ class SearchMovie extends Component {
 
 }
 
-export default SearchMovie; 
+export default SearchFor; 
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -5,16 +5,18 @@ import MovieApp from '../containers/landing/MovieApp';
 
 
 const Movie = lazy(() => import('../containers/movie/Movie'));
+const SearchFor = lazy() => import('../components/SearchFor/SearchFor')
 
 
 const Routes = () => {
   return (
     <Switch>
-      <Route path = '/' exact component={MovieApp} />
+      <Route path = '/' exact component={ MovieApp } />
       {/* <Route path = '/' exact component={movies} /> */}
       {/* <Route path = '/' exact component={tv} /> */}
       {/* <Route path = '/' exact component={mylist} /> */}
-      <Route exact path='/moviedetail/:id' component={Movie} />
+      <Route exact path='/moviedetail/:id' component={ Movie } />
+      <Route exact path='/searchfor' component={ SearchFor } />
     </Switch>
   )
 }
