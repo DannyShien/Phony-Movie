9b0a2a29f6bcd78038badb555e29c932bb23dc69
Stanley Chen <dannyshien@gmail.com>
2020-04-14 20:36:27 -0400

    Create SearchReslt DOM elements for page layout

--- src/components/searchResult/SearchResult.js
+++ src/components/searchResult/SearchResult.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
 const SearchResult = (props) => {
-  console.log(props.location.state)
-
+  // const searchResult = props.location.state
+  const renderResults = props.location.state.map((result, i) => {
+    console.log(result)
+    return (
+        <div className='resultPosters' key={result.id} style={{color: 'white'}}>
+          <img src={`https://image.tmdb.org/t/p/w300/${result.poster_path} `} />
+        </div>
+    )
+  } )
 
   return (
     <div className='result'>
-      <div 
-        className='resultContainer'
-        style={{
-          color: 'white'
-        }}
-      >
-        hello world!
+      <div className='resultContent'>
+        {renderResults}
       </div>
     </div>
   )
